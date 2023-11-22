abb39598ac3565eb4d4718d5170271d112ef5d73
wremsen <wcremsen@gmail.com>
2023-11-22 15:21:34 -0500

    phase 1 done

--- practice-for-ch-rails-first-routes-long-practice-main/first_routes/config/routes.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/first_routes/config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
 
+  get "users/", to: "users#index"
+  post "users/", to: "users#create"
+  get "users/new/", to: "users#new"
+  get "users/:id/edit", to: "users#edit"
+  get "users/:id/", to: "users#show"
+  patch "users/:id/", to: "users#update"
+  put "users/:id/", to: "users#update"
+  delete "users/:id/", to: "users#destroy"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-resources :users
+  # resources :users
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+
 end
